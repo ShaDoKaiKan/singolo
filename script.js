c058a670831b52b9a9c66809f7850a6daad11604
ShaDoKaiKan <sha.alia95@gmail.com>
2020-03-27 14:16:58 +0300

    feat: add task Singolo.Responsive

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const ANCHORS = document.querySelectorAll('.nav_othersS1');
 
 for(let anchor of ANCHORS) {
     anchor.addEventListener("click", (e) => {
-        e.preventDefault();
         const blockID = anchor.getAttribute('href');
         document.querySelector(blockID).scrollIntoView({
             behavior: "smooth",
@@ -16,15 +15,14 @@ for(let anchor of ANCHORS) {
 let sections = document.querySelectorAll('section');
 
 document.addEventListener('scroll', e => {
+    
     const currentScrollY = window.scrollY + document.querySelector('header').offsetHeight;
+    const contactPosition = document.getElementById('contact').offsetTop;
+    const currentPosition = window.pageYOffset;
+
+    
     sections.forEach( (section) => {
         
-        if (window.innerHeight + window.scrollY == document.body.offsetHeight) {
-            document.querySelectorAll('#menu a').forEach((el) => {
-                el.classList.remove('active');
-            })
-            document.getElementById('last-child').classList.add('active');
-        }
         if (section.offsetTop <= currentScrollY && (section.offsetTop + section.offsetHeight) > currentScrollY) {
             document.querySelectorAll('#menu a').forEach ( (el) => {
                 el.classList.remove('active');
@@ -32,106 +30,85 @@ document.addEventListener('scroll', e => {
                     el.classList.add('active');
                 }
             });
-        }
-        
-        
+        }  
+
     });
+    if (currentPosition + 400 >= contactPosition) {
+        document.querySelectorAll('#menu a').forEach((el) => {
+            el.classList.remove('active');
+        })
+        document.getElementById('last-child1').classList.add('active');
+        document.getElementById('last-child2').classList.add('active');
+    }
+    
 });
 
 
 
 //----------------------------------SLIDER-------------------------------------------------
-const LEFT_ARROW = document.getElementById('left-arrow');
-const RIGHT_ARROW = document.getElementById('right-arrow');
-const SlidersIdList = ['slider-red', 'slider-blue'];
-let indexOfActiveSlide = 0;
+let items = document.querySelectorAll('.item');
+let currentItem = 0;
 let isEnabled = true;
 
-LEFT_ARROW.addEventListener('click', (e) => {
+function changeCurrentItem(n) {
+    currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+    isEnabled = false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove('active-slider', direction);
+    })
+}
+
+function showItem(direction) {
+    items[currentItem].classList.add('next', direction);
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove('next', direction);
+        this.classList.add('active-slider');
+        isEnabled = true;
+    })
+}
+
+function previousItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n - 1);
+    showItem('from-left');
+}
+
+function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n + 1);
+    showItem('from-right');
+}
+document.querySelector('.arrow-right').addEventListener('click', function() {
     if (isEnabled) {
-        isEnabled = false;
-        if (indexOfActiveSlide == 0) {
-            document.getElementById('slider-blue').style.left = "-2040px";
-            document.getElementById('sliderS1').style.backgroundColor = '#648bf0';
-            document.getElementById('slider-bt').style.backgroundColor = '#648bf0';
-            indexOfActiveSlide = 1;
-        } else {
-            document.getElementById('slider-red').style.left = "-1020px";
-            document.getElementById('sliderS1').style.backgroundColor = '#f06c64';
-            document.getElementById('slider-bt').style.backgroundColor = '#f06c64';
-            indexOfActiveSlide = 0;
-        }
-        const slider = setInterval(() => {
-            if (indexOfActiveSlide == 1) {
-                document.getElementById('slider-red').style.left = +document.getElementById('slider-red').style.left.replace('px', '') + 30 + 'px';
-                document.getElementById('slider-blue').style.left = +document.getElementById('slider-blue').style.left.replace('px', '') + 30 + 'px';
-                if (document.getElementById(`${SlidersIdList[0]}`).style.left == '1020px') {
-                    isEnabled = true;
-                    clearInterval(slider);
-                }
-            } else {
-                document.getElementById('slider-blue').style.left = +document.getElementById('slider-blue').style.left.replace('px', '') + 30 + 'px';
-                document.getElementById('slider-red').style.left = +document.getElementById('slider-red').style.left.replace('px', '') + 30 + 'px';
-                if (document.getElementById('slider-red').style.left == '0px') {
-                    isEnabled = true;
-                    clearInterval(slider);
-                }
-            }
-        }, 10);
-    }  
+        previousItem(currentItem)
+    }
 });
-
-RIGHT_ARROW.addEventListener('click', (e) => {
+document.querySelector('.arrow-left').addEventListener('click', function() {
     if (isEnabled) {
-        isEnabled = false;
-        if (indexOfActiveSlide == 0) {
-            document.getElementById('slider-blue').style.left = "0px";
-            indexOfActiveSlide = 1;
-            document.getElementById('sliderS1').style.backgroundColor = '#648bf0';
-            document.getElementById('slider-bt').style.backgroundColor = '#648bf0';
-        } else {
-            document.getElementById('slider-red').style.left = "1020px";
-            indexOfActiveSlide = 0;
-            document.getElementById('sliderS1').style.backgroundColor = '#f06c64';
-            document.getElementById('slider-bt').style.backgroundColor = '#f06c64';
-        }
-        const slider = setInterval(() => {
-            if (indexOfActiveSlide == 1) {
-                document.getElementById('slider-red').style.left = +document.getElementById('slider-red').style.left.replace('px', '') - 30 + 'px';
-                document.getElementById('slider-blue').style.left = +document.getElementById('slider-blue').style.left.replace('px', '') - 30 + 'px';
-                if (document.getElementById(`${SlidersIdList[0]}`).style.left == '-1020px') {
-                    isEnabled = true;
-                    clearInterval(slider);
-                }
-            } else {
-                document.getElementById('slider-blue').style.left = +document.getElementById('slider-blue').style.left.replace('px', '') - 30 + 'px';
-                document.getElementById('slider-red').style.left = +document.getElementById('slider-red').style.left.replace('px', '') - 30 + 'px';
-                if (document.getElementById('slider-red').style.left == '0px') {
-                    isEnabled = true;
-                    clearInterval(slider);
-                }
-            }
-        }, 10);
+        nextItem(currentItem)
     }
 });
 
-document.getElementById('home-btn-v').addEventListener('click', (event) => {
-    let screen_v = document.querySelector('.black-screen-v');
-    if (screen_v.style.visibility === 'visible') {
-        screen_v.style.visibility = 'hidden';
-    } else {
-        screen_v.style.visibility = 'visible';
-    }
-});
+// ------------------------------ Phone screen --------------------
+const PHONE_WALLPAPER_ONE = document.getElementById('black-one');
+const PHONE_WALLPAPER_TWO = document.getElementById('black-two');
+const delWallpaperOne = () => {
+    if (PHONE_WALLPAPER_ONE.classList.contains('none'))
+        PHONE_WALLPAPER_ONE.classList.remove('none')
+    else
+        PHONE_WALLPAPER_ONE.classList.add('none');
+}
+const delWallpaperTwo = () => {
+    if (PHONE_WALLPAPER_TWO.classList.contains('none'))
+        PHONE_WALLPAPER_TWO.classList.remove('none')
+    else
+        PHONE_WALLPAPER_TWO.classList.add('none');
+}
 
-document.getElementById('home-btn-h').addEventListener('click', (event) => {
-    let screen_h = document.querySelector('.black-screen-h');
-    if (screen_h.style.visibility === 'visible') {
-        screen_h.style.visibility = 'hidden';
-    } else {
-        screen_h.style.visibility = 'visible';
-    }
-});
 //----------------------------------PORTFOLIO--------------------------------------------
 const PORTFOLIO_MENU = document.getElementById('portfolio-menu');
 const IMAGES = document.getElementById('images');
@@ -147,8 +124,6 @@ for(let tab of tabs) {
         }
     });
 }
-
-
 
 IMAGES.addEventListener('click', (event) => {
     event.preventDefault();
